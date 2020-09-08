bda17b3503d8b486a28c411f63a355e330fb67a3
Steven <jiangyan@top-list.com.cn>
2020-09-08 15:28:01 +0800

    修复代码构建问题

--- web-starter/src/main/java/com/touchbiz/web/starter/servlet/ServletConfig.java
+++ web-starter/src/main/java/com/touchbiz/web/starter/servlet/ServletConfig.java
@@ -1,32 +1,23 @@
-//package com.touchbiz.web.starter.servlet;
-//
-////import com.touchbiz.web.starter.servlet.GracefulShutdownTomcat;
-//
-//import io.undertow.Undertow;
-//import io.undertow.UndertowOptions;
-//import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-//import org.springframework.boot.web.embedded.undertow.UndertowServletWebServerFactory;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.stereotype.Component;
-//
-//@Component
-//public class ServletConfig {
-//
-////    @ConditionalOnClass(TomcatServletWebServerFactory.class)
-////    @Bean
-////    public ServletWebServerFactory servletContainer(GracefulShutdownTomcat shutdown) {
-////        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
-////        tomcat.addConnectorCustomizers(shutdown);
-////        return tomcat;
-////    }
-//
-//    @ConditionalOnClass(Undertow.class)
-//    @Bean
-//    public UndertowServletWebServerFactory servletWebServerFactory(GracefulShutdownUndertowWrapper wapper) {
-//        UndertowServletWebServerFactory factory = new UndertowServletWebServerFactory();
-//        factory.addDeploymentInfoCustomizers(deploymentInfo -> deploymentInfo.addOuterHandlerChainWrapper(wapper));
-//        factory.addBuilderCustomizers(builder -> builder.setServerOption(UndertowOptions.ENABLE_STATISTICS, true));
-//        return factory;
-//    }
-//
-//}
+package com.touchbiz.web.starter.servlet;
+
+import io.undertow.Undertow;
+import io.undertow.UndertowOptions;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.web.embedded.undertow.UndertowServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ServletConfig {
+
+
+    @ConditionalOnClass(Undertow.class)
+    @Bean
+    public UndertowServletWebServerFactory servletWebServerFactory(GracefulShutdownUndertowWrapper wapper) {
+        UndertowServletWebServerFactory factory = new UndertowServletWebServerFactory();
+        factory.addDeploymentInfoCustomizers(deploymentInfo -> deploymentInfo.addOuterHandlerChainWrapper(wapper));
+        factory.addBuilderCustomizers(builder -> builder.setServerOption(UndertowOptions.ENABLE_STATISTICS, true));
+        return factory;
+    }
+
+}
