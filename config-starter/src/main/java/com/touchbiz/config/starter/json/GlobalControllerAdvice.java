c246e469fb9a03f1d871bf49fe4f326657fa8f2d
Steven <mrjiangyan@hotmail.com>
2021-06-17 17:29:42 +0800

    代码调整

---
+++ config-starter/src/main/java/com/touchbiz/config/starter/json/GlobalControllerAdvice.java
@@ -0,0 +1,32 @@
+package com.touchbiz.config.starter.json;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.InitBinder;
+
+import java.beans.PropertyEditorSupport;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Slf4j
+@ControllerAdvice
+public class GlobalControllerAdvice {
+
+    @InitBinder
+    public void initBinder(WebDataBinder webDataBinder) {
+        webDataBinder.registerCustomEditor(LocalDate.class, new PropertyEditorSupport() {
+            @Override
+            public void setAsText(String text) throws IllegalArgumentException {
+                setValue(LocalDate.parse(text, DateTimeFormatter.ISO_DATE));
+            }
+        });
+        webDataBinder.registerCustomEditor(LocalDateTime.class, new PropertyEditorSupport() {
+            @Override
+            public void setAsText(String text) throws IllegalArgumentException {
+                setValue(LocalDateTime.parse(text, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+            }
+        });
+    }
+}
