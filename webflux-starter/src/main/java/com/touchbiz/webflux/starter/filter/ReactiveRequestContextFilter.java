b912169d2fb8a9bcfb12329c1dc6572c8ad7b8b4
Steven <mrjiangyan@hotmail.com>
2021-07-09 16:08:04 +0800

    代码调整

--- webflux-starter/src/main/java/com/touchbiz/webflux/starter/filter/ReactiveRequestContextFilter.java
+++ webflux-starter/src/main/java/com/touchbiz/webflux/starter/filter/ReactiveRequestContextFilter.java
@@ -13,23 +13,23 @@ import reactor.core.publisher.SignalType;
 
 import java.util.Map;
 
-//@Configuration
-//@ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.REACTIVE)
+@Configuration
+@ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.REACTIVE)
 public class ReactiveRequestContextFilter implements WebFilter , Ordered {
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
         ServerHttpRequest request = exchange.getRequest();
         ReactiveRequestContextHolder.put(request);
-        if(exchange.getAttributes() != null){
-            Map map = exchange.getAttributes();
-            if(map.containsKey(HttpHeaderConstants.HEADER_USER)){
-                ReactiveRequestContextHolder.putUser(map.get(HttpHeaderConstants.HEADER_USER));
-            }
-            if(map.containsKey(HttpHeaderConstants.HEADER_CHANNEL)){
-                ReactiveRequestContextHolder.putChannel(Integer.parseInt(map.get(HttpHeaderConstants.HEADER_CHANNEL).toString()));
-            }
-        }
+//        if(exchange.getAttributes() != null){
+//            Map map = exchange.getAttributes();
+//            if(map.containsKey(HttpHeaderConstants.HEADER_USER)){
+//                ReactiveRequestContextHolder.putUser(map.get(HttpHeaderConstants.HEADER_USER));
+//            }
+//            if(map.containsKey(HttpHeaderConstants.HEADER_CHANNEL)){
+//                ReactiveRequestContextHolder.putChannel(Integer.parseInt(map.get(HttpHeaderConstants.HEADER_CHANNEL).toString()));
+//            }
+//        }
         return chain.filter(exchange)
                 .doFinally(signalType -> {
                     if (SignalType.ON_COMPLETE == signalType) {
