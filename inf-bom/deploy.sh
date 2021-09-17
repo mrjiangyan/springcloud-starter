c49bbc031a010b93d0131faad18fe59f255aae20
Steven <mrjiangyan@hotmail.com>
2021-09-17 13:01:04 +0800

    kotlin demo

--- inf-bom/deploy.sh
+++ inf-bom/deploy.sh
@@ -1,4 +1,1 @@
-#mvn deploy:deploy-file -DgroupId=com.touchbiz -DartifactId=inf-bom -Dversion=$1 -Dpackaging=pom -Dfile=inf-bom.xml  -Durl=https://packages.aliyun.com/maven/repository/2031284-release-KDf3XN -DrepositoryId=rdc-touchbiz-releases
-
-mvn deploy:deploy-file -DgroupId=com.touchbiz -DartifactId=inf-bom -Dversion=$1 -Dpackaging=pom -Dfile=inf-bom.xml  -Durl=https://packages.aliyun.com/maven/repository/2041383-release-F0cHwt -DrepositoryId=rdc-releases
-
+mvn deploy:deploy-file -DgroupId=com.touchbiz -DartifactId=inf-bom -Dversion=$1 -Dpackaging=pom -Dfile=inf-bom.xml  -Durl=https://packages.aliyun.com/maven/repository/2031284-release-KDf3XN -DrepositoryId=rdc-touchbiz-releases
