c1042617ddb547e19a1b82c8510e37ee178f243e
DESKTOP-NTOSLCT\steven <mrjiangyan@hotmail.com>
2021-09-17 12:42:52 +0800

    构建完全成功的版本

---
+++ inf-bom/deploy-starter.sh
@@ -0,0 +1,4 @@
+#mvn deploy:deploy-file -DgroupId=com.touchbiz -DartifactId=inf-bom -Dversion=$1 -Dpackaging=pom -Dfile=inf-bom.xml  -Durl=https://packages.aliyun.com/maven/repository/2031284-release-KDf3XN -DrepositoryId=rdc-touchbiz-releases
+
+mvn deploy:deploy-file -DgroupId=com.touchbiz -DartifactId=starter-parent -Dversion=1.0 -Dpackaging=pom -Dfile=starter-parent.xml  -Durl=https://packages.aliyun.com/maven/repository/2031284-release-KDf3XN -DrepositoryId=rdc-releases
+
