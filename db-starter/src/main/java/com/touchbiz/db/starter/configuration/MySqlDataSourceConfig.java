327860aa47556f972e677f425033b433fa99b71d
湃道智能科技 <yan.jiang@ai-prime.ai>
2021-12-23 20:53:41 +0800

    代码打包上传问题解决

--- db-starter/src/main/java/com/touchbiz/db/starter/configuration/MySqlDataSourceConfig.java
+++ db-starter/src/main/java/com/touchbiz/db/starter/configuration/MySqlDataSourceConfig.java
@@ -1,24 +1,24 @@
-package com.touchbiz.db.starter.configuration;
-
-import lombok.Data;
-
-@Data
-public class MySqlDataSourceConfig {
-
-    private String dbUrl;
-
-    private String username;
-
-    private String password;
-
-    private String driverClassName;
-
-    private int initialSize;
-
-    private int minIdle;
-
-    private int maxActive;
-
-    private int maxWait;
-
-}
+//package com.touchbiz.db.starter.configuration;
+//
+//import lombok.Data;
+//
+//@Data
+//public class MySqlDataSourceConfig {
+//
+//    private String dbUrl;
+//
+//    private String username;
+//
+//    private String password;
+//
+//    private String driverClassName;
+//
+//    private int initialSize;
+//
+//    private int minIdle;
+//
+//    private int maxActive;
+//
+//    private int maxWait;
+//
+//}
