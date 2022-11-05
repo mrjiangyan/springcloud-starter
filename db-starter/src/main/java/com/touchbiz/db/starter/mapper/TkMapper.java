705f9efc2eeddf54ffb55d47a9427452e1ab2c2e
steven <mrjiangyan@hotmail.com>
2022-11-05 22:46:03 +0800

    代码优化

--- db-starter/src/main/java/com/touchbiz/db/starter/mapper/TkMapper.java
+++ db-starter/src/main/java/com/touchbiz/db/starter/mapper/TkMapper.java
@@ -1,12 +1,12 @@
-package com.touchbiz.db.starter.mapper;
-
-import tk.mybatis.mapper.common.ConditionMapper;
-import tk.mybatis.mapper.common.IdsMapper;
-import tk.mybatis.mapper.common.Mapper;
-import tk.mybatis.mapper.common.MySqlMapper;
-
-
-public interface TkMapper<T> extends Mapper<T>, MySqlMapper<T>, IdsMapper<T>, ConditionMapper<T> {
-    // TODO
-    // FIXME 特别注意，该接口不能被扫描到，否则会出错
-}
+//package com.touchbiz.db.starter.mapper;
+//
+//import tk.mybatis.mapper.common.ConditionMapper;
+//import tk.mybatis.mapper.common.IdsMapper;
+//import tk.mybatis.mapper.common.Mapper;
+//import tk.mybatis.mapper.common.MySqlMapper;
+//
+//
+//public interface TkMapper<T> extends Mapper<T>, MySqlMapper<T>, IdsMapper<T>, ConditionMapper<T> {
+//    // TODO
+//    // FIXME 特别注意，该接口不能被扫描到，否则会出错
+//}
